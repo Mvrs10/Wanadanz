9651e6f32c0c143a83ca43ae42ae0e44b243cc38
Minh Khoi Phan <pmka42@gmail.com>
2025-07-01 16:36:33 -0400

    Set stage.

--- Assets/Scripts/RadioController.cs
+++ Assets/Scripts/RadioController.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class RadioController : MonoBehaviour
 {
     public GameObject promptCanvas;
+    public GameObject piano, guitar, amp;
     public Transform player;
     public new CameraController camera;
     private bool isInteractable = false;
@@ -29,13 +30,23 @@ public class RadioController : MonoBehaviour
     {
         if (isInteractable && Input.GetKeyDown(KeyCode.E))
         {
+            //Setup postion
             player.position = new Vector3(0f, 4.4f, 0f);
             player.rotation = Quaternion.Euler(0f, 90f, 0f);
-            //promptCanvas.SetActive(false);
+            piano.SetActive(true);
+            guitar.SetActive(true);
+            amp.SetActive(true);           
 
             camera.DisableFollow();
             Camera.main.transform.position = new Vector3(5f,6f,0f);
             Camera.main.transform.LookAt(player.position + Vector3.up * 1.5f);
+
+            //Freeze my player
+            Rigidbody rb = player.GetComponent<Rigidbody>();
+            rb.constraints = RigidbodyConstraints.FreezeAll;
+
+            //Start the music
+            SoundManager.PlaySound(SoundType.MUSIC);
         }
     }
 }
